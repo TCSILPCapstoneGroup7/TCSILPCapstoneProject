7cfcd8aaf54457a04c6854c779938a7414cafde6
Venk <vkunduru01@gmail.com>
2021-09-15 17:48:30 -0400

    debugging

--- Cap_Front_End/capstone-shopping/src/app/app.component.ts
+++ Cap_Front_End/capstone-shopping/src/app/app.component.ts
@@ -9,22 +9,4 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'capstone-shopping';
-
-  requestRef = new FormGroup({
-    productname: new FormControl(""),
-    quantityincreased: new FormControl("")
-  })
-  msg: String = "";
-
-  sendRequest(): void {
-    console.log("HELLO");
-    // let request = this.requestForm.value
-    // productRequestModel.insertMany( {productname: request.productname, quantityincreased: request.quantityincreased}, (err,result) =>{
-    //   if (!err) {
-    //     console.log("Course stored successfully.")
-    //   } else {
-    //     console.log(err);
-    //   }
-    // })
-  }
 }
